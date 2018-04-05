42f693865bd73b1435bf1990d43aa7894256a54b
waelbendhiadigitus <wael@localhost.localdomain>
2018-04-05 01:12:48 +0100

    Add solution to problem 100

---
+++ src/Level5/Problem100.hs
@@ -0,0 +1,81 @@
+module Level5.Problem100
+  ( problem
+  ) where
+
+import Data.List
+import Data.Maybe
+
+import Problem
+
+problem :: Problem Integer
+problem =
+  Problem
+  { ind = 100
+  , name = "Arranged probability"
+  , solution = fst $ head $ dropWhile ((< 10 ^ 12) . snd) solutions'
+  }
+
+solvePolynomial :: Integral t => t -> t -> t -> Maybe t
+solvePolynomial a b c = do
+  d <- safeSqrt $ discriminant
+  safeDiv
+    (if d > b
+       then d - b
+       else -d - b)
+    (2 * a)
+  where
+    discriminant = b ^ 2 - 4 * a * c
+
+safeSqrt :: Integral a => a -> Maybe a
+safeSqrt x
+  | x < 0 = Nothing
+  | root ^ 2 == x = Just root
+  | otherwise = Nothing
+  where
+    root = truncate $ sqrt $ fromIntegral x
+
+safeDiv :: Integral a => a -> a -> Maybe a
+safeDiv a b =
+  if a `rem` b == 0
+    then Just $ a `div` b
+    else Nothing
+
+findFirstCombination :: Integral a => a -> Maybe a
+findFirstCombination bound = find ((/= Nothing) . blueBalls) $ [bound ..]
+
+blueBalls :: Integral t => t -> Maybe t
+blueBalls total = solvePolynomial 2 (-2) (total - (total ^ 2))
+
+-- (x / y) * (x-1/(y-1)) = 1/2
+-- 2*x^2 - 2*x + y - y^2 = 0
+-- disc = 4(2y^2 - 2y + 1)
+-- s1 = (1 + sqrt(2y^2 - 2y + 1))/2
+-- s2 = (1 - sqrt(2y^2 - 2y + 1))/2
+-- 2y^2 - 2y  > -1
+-- 2y - 2y^2 < 1
+-- y < 1/2 + y^2
+--
+-- Integer
+-- integer                        -> (1 + sqrt(2y^2 - 2y + 1)) / 2
+-- even                           -> 1 + sqrt(2y^2 - 2y + 1)
+-- odd                            -> sqrt(2y^2 - 2y + 1)
+-- odd perfect square             -> 2y^2 - 2y + 1
+-- (odd perfect square) - 1       -> 2y^2 - 2y
+-- ((odd perfect square) - 1) / 2 -> y^2 - y
+-- y^2 - y - ps
+solutions :: [(Integer, Integer)]
+solutions = do
+  t <-
+    [1,3 ..] >>=
+    maybeToList . solvePolynomial 1 (-1) . flip div 2 . (-) 1 . (^ 2)
+  b <- maybeToList $ blueBalls t
+  return (b, t)
+
+-- After much fiddling about I gave up and googled '"quadratic two integer
+-- equation" which lead me here https://www.alpertron.com.ar/QUAD.HTM
+-- which I shamefully just copy pasted.
+-- I left the original solution for posterity.
+solutions' :: [(Integer, Integer)]
+solutions' = (15, 21) : map next solutions'
+  where
+    next (blue, total) = (3 * blue + 2 * total - 2, 4 * blue + 3 * total - 3)
--- src/Level5/Solutions.hs
+++ src/Level5/Solutions.hs
@@ -2,6 +2,7 @@ module Level5.Solutions
   ( solutions
   ) where
 
+import qualified Level5.Problem100 as P100
 import qualified Level5.Problem102 as P102
 import qualified Level5.Problem104 as P104
 import qualified Level5.Problem112 as P112
@@ -9,4 +10,4 @@ import qualified Level5.Problem112 as P112
 import Problem
 
 solutions :: [Problem Integer]
-solutions = [P102.problem, P104.problem, P112.problem]
+solutions = [P100.problem, P102.problem, P104.problem, P112.problem]
