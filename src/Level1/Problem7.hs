13cb526f01ba058aed4fc5a4ba9a778b1fab5785
waelbendhiadigitus <wael@localhost.localdomain>
2018-04-26 16:45:10 +0100

    Rewrite problem 7 with simple prime sieve

--- src/Level1/Problem7.hs
+++ src/Level1/Problem7.hs
@@ -2,8 +2,28 @@ module Level1.Problem7
   ( problem
   ) where
 
-import Data.Numbers.Primes
+import Data.Array
+
 import Problem
 
 problem :: Problem Integer
-problem = Problem {ind = 7, name = "10001st prime", solution = primes !! 10000}
+problem = Problem {ind = 7, name = "10001st prime", solution = nthPrime 10001}
+
+nthPrime :: (Integral e, Num a, Ix a, Enum a) => a -> e
+nthPrime n = arr ! (n - 1)
+  where
+    arr = listArray (0, (n - 1)) (map f [0 .. n - 1])
+    f x
+      | x == 0 = 2
+      | otherwise =
+        head $
+        filter (\c -> all (\p -> c `rem` p /= 0) lowerPrimes) $
+        [nextOdd,nextOdd + 2 ..]
+      where
+        prev = arr ! (x - 1)
+        nextOdd =
+          (prev +) $
+          if even prev
+            then 1
+            else 2
+        lowerPrimes = map (arr !) [0 .. x - 1]
