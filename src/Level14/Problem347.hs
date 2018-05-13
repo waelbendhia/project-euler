ad7d05041f83974445166683d9809cf241e30e8d
waelbendhiadigitus <wael@localhost.localdomain>
2018-05-13 12:38:05 +0100

    Add solution to problem 347

---
+++ src/Level14/Problem347.hs
@@ -0,0 +1,34 @@
+module Level14.Problem347
+  ( problem
+  ) where
+
+import Data.List
+import Data.Numbers.Primes
+
+import Problem
+
+problem :: Problem Integer
+problem =
+  Problem
+  { ind = 347
+  , name = "Largest integer divisible by two primes"
+  , solution = capitalS (10 ^ 7)
+  }
+
+capitalS n = helper primes
+  where
+    capitalM' p q = capitalM p q n
+    helper [] = 0
+    helper (p:ps)
+      | p * p <= n =
+        (sum $ capitalM' p <$> takeWhile ((<= n) . (* p)) ps) + helper ps
+      | otherwise = 0
+
+capitalM :: (Num p, Ord p) => p -> p -> p -> p
+capitalM p q n =
+  if length ns > 0
+    then maximum ns
+    else 0
+  where
+    ps = takeWhile (<= n) $ map (p ^) [1 ..]
+    ns = ps >>= \p' -> takeWhile (<= n) $ ((p' *) . (q ^)) <$> [1 ..]
--- src/Level14/Solutions.hs
+++ src/Level14/Solutions.hs
@@ -4,8 +4,9 @@ module Level14.Solutions
 
 import qualified Level14.Problem345 as P345
 import qualified Level14.Problem346 as P346
+import qualified Level14.Problem347 as P347
 
 import Problem
 
 solutions :: [Problem Integer]
-solutions = [P345.problem, P346.problem]
+solutions = [P345.problem, P346.problem, P347.problem]
